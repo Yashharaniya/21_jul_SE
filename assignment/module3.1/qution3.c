a9e4ec62d81a65c7bb4f2888861b5d69d469bc41
Yashharaniya <109731934+Yashharaniya@users.noreply.github.com>
2022-08-25 12:58:46 +0530

    Add files via upload

---
+++ assignment/module3.1/qution3.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float pi=3.14;
+    float re,area;
+     
+    printf("enter the valu\n");
+    scanf("%f",&re);
+    area=pi*re*re;
+    printf("arae of the curcal is: %f\n",area);
+
+    return 0;
+}
