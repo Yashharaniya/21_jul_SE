16e343f506cf3c86a4ff6915f11b99403d38673c
Yashharaniya <109731934+Yashharaniya@users.noreply.github.com>
2022-09-01 17:19:03 +0530

    Add files via upload

---
+++ tasks/piramit.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int i,j,num=1,a,b;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;++j)
+    
+        {
+            printf("%d ",num);
+            ++num;
+    }
+    printf("\n");
+}
+   for(a=10;a>=6;a--)
+    {
+        for(b=6;b<=a;b++)
+    
+        {
+            printf("%d ",num);
+            ++num;
+    }
+    printf("\n");
+}
+}
+
+
+
