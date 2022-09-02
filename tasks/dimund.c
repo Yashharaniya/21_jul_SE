3a2ea17fcc5d03a2a5aef0f65fa10493feaccdc0
Yashharaniya <109731934+Yashharaniya@users.noreply.github.com>
2022-09-02 12:32:39 +0530

    Add files via upload

---
+++ tasks/dimund.c
@@ -0,0 +1,32 @@
+
+#include<stdio.h>
+
+int main()
+{
+	int i,j,a;
+	for(i=1;i<=5;i++)
+	{
+		for(a=5;a>=i;a--)
+		{
+		printf(" ");
+            	}
+		for(a=1;a<=i;a++)
+		{
+			printf("%d ",a);
+		}
+		printf("\n");
+}
+	for(i=5;i>=1;i--)
+	{
+		for(a=5;a>=i;a--)
+		{
+		printf(" ");
+            	}
+		for(a=1;a<=i;a++)
+		{
+			printf("%d ",a);
+		}
+		printf("\n");
+}
+}
+        
