fa12b4a4cb164f78fc5cdcfda15aa99b16cae1bc
Yashharaniya <109731934+Yashharaniya@users.noreply.github.com>
2022-09-07 15:24:40 +0530

    Add files via upload

---
+++ assignment/modul 3.2/monday to sunday.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+
+int
+main ()
+{
+  int days;
+   printf("enter the number");
+  scanf ("%d", &days);
+ 
+  switch (days){
+  case 1:
+  {
+      printf("today is monday ");
+  }
+  break;
+   case 2:
+  {
+      printf("today is tushday");
+  }
+  break;
+   case 3:
+  {
+      printf("today is wedshday ");
+  }
+  break;
+   case 4:
+  {
+      printf("today is tushday ");
+  }
+  break;
+   case 5:
+  {
+      printf("today is friday ");
+  }
+  break;
+   case 6:
+  {
+      printf("today is seterday ");
+  }
+  break;
+   case 7:
+  {
+      printf("today is sunday ");
+  }
+  break;
+  default :{
+  printf("enter valed number");
+      
+  
+  }
+     return 0;
+}
+}
