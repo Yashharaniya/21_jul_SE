44097d7bd46e75f7d0fd893d1be916ee46b98f59
Yashharaniya <109731934+Yashharaniya@users.noreply.github.com>
2022-09-07 16:10:12 +0530

    Add files via upload

---
+++ assignment/modul 3.2/978 to 845 loop.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    for(a=972;a>897;a--)
+    {
+        printf("%d\n",a);
+    }
+
+}
