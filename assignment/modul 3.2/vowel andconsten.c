a474b9b6ece3e7fd90288ea67d22fbc00d8b50c2
Yashharaniya <109731934+Yashharaniya@users.noreply.github.com>
2022-09-07 15:55:42 +0530

    Add files via upload

---
+++ assignment/modul 3.2/vowel andconsten.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+int
+main ()
+{
+  char ch;
+  printf ("enter the varibal");
+  scanf ("%c", &ch);
+  switch (ch)
+    {
+    case 'a':
+      {
+	printf ("vowel");
+      }
+      break;
+    case 'e':
+      {
+	printf ("vowel");
+      }
+      break;
+    case 'i':
+      {
+	printf ("vowel");
+      }
+      break;
+    case 'o':
+      {
+	printf ("vowel");
+      }
+      break;
+    case 'u':
+      {
+	printf ("vowel");
+      }
+      break;
+    default:
+      {
+	printf ("consonant");
+      }
+    }
+}
