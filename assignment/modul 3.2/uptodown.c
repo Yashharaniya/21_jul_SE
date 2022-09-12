f2e42256cfe5117d8054948ce0d63620389077a9
Yashharaniya <109731934+Yashharaniya@users.noreply.github.com>
2022-09-12 16:15:21 +0530

    Add files via upload

---
+++ assignment/modul 3.2/uptodown.c
@@ -0,0 +1,30 @@
+//*
+//**
+//***
+//****
+//*****
+//****
+//***
+//**
+//*
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    
+    for(a=0;a<6;a++){
+        for(b=0;b<a;b++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(a=6;a>0;a--){
+        for(b=0;b<a;b++){
+            printf("*");
+        }
+        printf("\n");
+    }
+   
+    
+    }
