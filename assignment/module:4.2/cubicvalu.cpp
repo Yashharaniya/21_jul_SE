5dafd327ad05cbab5a9f85671768d43456179f66
Yashharaniya <109731934+Yashharaniya@users.noreply.github.com>
2022-11-09 15:23:00 +0530

    Add files via upload

---
+++ assignment/module:4.2/cubicvalu.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using  namespace std;
+#include<conio.h>
+  
+class line
+{
+   public:
+              inline float mul(float x,float y)
+              {
+                            return(x*y);
+              }
+              inline float cube(float x)
+              {
+                            return(x*x*x);
+              }
+};
+ 
+int main()
+{
+              line obj;
+              float val1,val2;
+              
+              cout<<"Enter two values:";
+              cin>>val1>>val2;
+              cout<<"\nMultiplication value is:"<<obj.mul(val1,val2);
+              cout<<"\n\nCube value is          :"<<obj.cube(val1)<<"\t"<<obj.cube(val2);
+              return 0;
+}
